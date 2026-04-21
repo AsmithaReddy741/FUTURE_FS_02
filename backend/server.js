472323e8b7ad3652cffb41b3d4eeb88e3a435a6b
Asmitha Dyapa <dyapaasmitha@gmail.com>
2026-04-21 21:38:44 +0530

    fixed mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,11 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect("mongodb://127.0.0.1:27017/crm")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running on port " + PORT));
 
 // Schema
 const LeadSchema = new mongoose.Schema({
